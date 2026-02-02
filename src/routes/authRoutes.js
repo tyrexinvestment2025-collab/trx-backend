c8c7b6bbba34ca78a253f464f4a7566a3f315e3e
tyrexinvestment2025-collab <tyrexinvestment2025@gmail.com>
2026-02-02 12:43:12 +0100

    fix

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { login } = require('../controllers/AuthController');
+const { login } = require('../controllers/authController');
 
 router.post('/login', login);
 
